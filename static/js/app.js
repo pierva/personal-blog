03c6a837d426cb53762fad039a387f0b564ac70b
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-13 23:11:21 -0500

    feat: Add transition on navbar hide/show

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,15 @@
 // Data is stored in the model
-let model = {}
+let model = {
+    NAVHEIGHT: '100px'
+}
 
 // The octopus is the link between the model and the view
 let octopus = {
     init: function() {
         view.init();
+    },
+    getNavbarHeight: function() {
+        return model.NAVHEIGHT;
     }
 }
 
@@ -19,17 +24,23 @@ let view = {
         let prevPosition = mainContainer.scrollTop;
         mainContainer.onscroll = function() {
             const currPosition = mainContainer.scrollTop;
-            if (prevPosition < currPosition) {
-                // nav.style.top = '-100px';
-                // main.style.top = '-50px';
-                nav.classList.add('nav-hide');
-                main.classList.add('nav-hide-move-content');
+            console.log('current', currPosition);
+            console.log('previous', prevPosition)
+            if (prevPosition - currPosition < 50) {
+                nav.style.top = '-' + octopus.getNavbarHeight();
+                main.style.top = '-50px';
+                // nav.classList.add('nav-hide');
+                // main.classList.add('nav-hide-move-content');
 
             } else {
-                nav.classList.remove('nav-hide');
-                main.classList.remove('nav-hide-move-content');
+                nav.style.top = '0';
+                main.style.top = '0';
+                // nav.classList.remove('nav-hide');
+                // main.classList.remove('nav-hide-move-content');
             }
-            prevPosition = currPosition;
+            if(currPosition - prevPosition > 50) {
+                prevPosition = currPosition;
+            }
         }
     }
 }
