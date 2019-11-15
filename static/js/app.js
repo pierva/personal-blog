5730f5d16a654a605b23a0e3d1425b33b79dd793
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-14 22:42:20 -0500

    feat: Show/Hide page scrolToTop button on scroll

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 // Data is stored in the model
 let model = {
-    NAVHEIGHT: '100px'
+    NAVHEIGHT: '100px',
+    scrollPosition: 0
+
 }
 
 // The octopus is the link between the model and the view
@@ -10,12 +12,19 @@ let octopus = {
     },
     getNavbarHeight: function() {
         return model.NAVHEIGHT;
+    },
+    getScrollPosition: function() {
+        return model.scrollPosition;
+    },
+    updateScrollPosition: function(pos) {
+        return model.scrollPosition = pos;
     }
 }
 
 let view = {
     init: function() {
         this.hideNavBarOnScroll();
+        this.toggleScrollMeBack(100);
     },
 
     hideNavBarOnScroll: function() {
@@ -23,7 +32,7 @@ let view = {
         const mainContainer = document.getElementById('main');
         let prevPosition = mainContainer.scrollTop;
         mainContainer.onscroll = function() {
-            const currPosition = mainContainer.scrollTop;
+            const currPosition = mainContainer.scrollTop;   
             if (prevPosition - currPosition < 50) {
                 nav.style.top = '-' + octopus.getNavbarHeight();
                 main.style.top = '-50px';
@@ -39,6 +48,21 @@ let view = {
             if(currPosition - prevPosition > 50) {
                 prevPosition = currPosition;
             }
+        }
+    },
+
+    toggleScrollMeBack: function(buffer) {
+        const mainContainer = document.getElementById('main');
+        mainContainer.onscroll = function() {
+            const currPosition = mainContainer.scrollTop;
+            const scroller = document.getElementById('scrollMeUp');
+            if (currPosition > buffer) {
+                scroller.classList.remove('d-none');
+            }
+            else {
+                scroller.classList.add('d-none');
+            }
+            octopus.updateScrollPosition(currPosition);
         }
     }
 }
