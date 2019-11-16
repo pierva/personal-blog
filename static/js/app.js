fb2cf60167e40032ded11f2516dc53f9ba6b4e79
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-16 17:42:30 -0500

    chore: Create authors dataset

--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,11 @@ let model = {
             id: "31ac8038-79a7-4470-9820-b6080017c16f",
             title: "How the best pizza is cooked",
             image: "./static/assets/img/_MG_2589.JPG",
-            content: "",
-            author: "Piervalerio Vignola",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing \
+                      elit, sed do eiusmod tempor incididunt ut labore \
+                      et dolore magna aliqua. Mauris vitae ultricies \
+                      leo integer malesuada nunc vel risus.",
+            authorId: "0599a05c-5363-4663-b482-9307ec9088f4",
             length: "Oct 19 - 5 min read",
             ratings: []
         },
@@ -16,8 +19,11 @@ let model = {
             id: "963a9b9b-5682-46fc-a0a5-c4b1638979a3",
             title: "How to get started with the mother yeast",
             image: "./static/assets/img/lievito_madre.jpg",
-            content: "",
-            author: "Bruno Rossi",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing \
+                      elit, sed do eiusmod tempor incididunt ut labore \
+                      et dolore magna aliqua. Mauris vitae ultricies \
+                      leo integer malesuada nunc vel risus.",
+            authorId: "e1a2990a-f4ee-4589-b49d-98a3723ea243",
             length: "Aug 15 - 5 min read",
             ratings: []
         },
@@ -25,8 +31,11 @@ let model = {
             id: "a2dc2967-2f8e-439a-bd97-bd8ee9423149",
             title: "How to prepare the perfect dough",
             image: "./static/assets/img/DoughJPG.JPG",
-            content: "",
-            author: "Giovanni Verdi",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing \
+                      elit, sed do eiusmod tempor incididunt ut labore \
+                      et dolore magna aliqua. Mauris vitae ultricies \
+                      leo integer malesuada nunc vel risus.",
+            author: "33550a9b-6da6-43d6-8587-80f24bc0a49b",
             length: "Aug 3 - 8 min read",
             ratings: []
         },
@@ -34,19 +43,45 @@ let model = {
             id: "4f1d1eb5-a44e-4290-9e1d-08508cb060c7",
             title: "The origins of crudaiola",
             image: "./static/assets/img/crudaiola.jpg",
-            content: "",
-            author: "Bruno Rossi",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing \
+                      elit, sed do eiusmod tempor incididunt ut labore \
+                      et dolore magna aliqua. Mauris vitae ultricies \
+                      leo integer malesuada nunc vel risus.",
+            author: "e1a2990a-f4ee-4589-b49d-98a3723ea243",
             length: "Oct 5 - 7 min read",
             ratings: []
         },
         {
             id: "15f78953-6298-4f03-9059-37ef83e7aa23",
             title: "All the secrets behind the bread",
-            image: "./static/assets/img/Bread.jpg",
-            content: "",
-            author: "Piervalerio Vignola",
+            image: ".static/assets/img/Bread.jpg",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing \
+                      elit, sed do eiusmod tempor incididunt ut labore \
+                      et dolore magna aliqua. Mauris vitae ultricies \
+                      leo integer malesuada nunc vel risus.",
+            author: "0599a05c-5363-4663-b482-9307ec9088f4",
             length: "Sep 27 - 4 min read",
             ratings: []
+        }
+    ],
+    authors: [
+        {
+            id: "0599a05c-5363-4663-b482-9307ec9088f4",
+            name: "Piervalerio Vignola",
+            memberSince: "2017-06-16T10:18:52+0000",
+            avatar: "./static/assets/img/avatar.jpg"
+        },
+        {
+            id: "33550a9b-6da6-43d6-8587-80f24bc0a49b",
+            name: "Giovanni Verdi",
+            memberSince: "2017-10-12T22:40:08+0000",
+            avatar: "./static/assets/img/avatar_general.png"
+        },
+        {
+            id: "e1a2990a-f4ee-4589-b49d-98a3723ea243",
+            name: "Bruno Rossi",
+            memberSince: "2018-03-04T07:34:52+0000",
+            avatar: "./static/assets/img/avatar_general.png"
         }
     ]
 }
